237884583f12a49847e83f9229d2a47eecdf4c56
zymaislam <zyma.islam90@gmail.com>
2015-04-05 23:38:53 -0400

    Changed style of wholeQuote and mainQuote to matched that of Buzzfeed
    http://www.buzzfeed.com/briangalindo/struggles-that-were-all-too-real-to
    -90s-kids#.slB6gNBww

---
+++ scripts.js
@@ -0,0 +1,26 @@
+for (var i=0; i<data.length; i++){
+	
+	var rowData = data[i];
+	
+	var template =
+	
+	
+	'<div class="quoteContainer"></div>'+
+
+				'<div class="mainQuote">
+					For the past 26 years I've thought of heroin every day
+				</div>'+
+				'<div class="wholeQuote">
+					But never thought of using it. It never made anything better, just worse. Look, every junky is always "considering" getting clean. Every junky wants to be clean. Every using junky also wants more heroin. Call it a contradiction call it what you want. But no junky wants the life they have.
+				</div>'+
+				'<div class="source">
+					Katie
+				</div>'+
+				'<div class="yearsClean">
+					17 years clean
+				</div>'+
+				'<div class="location">
+					London,UK
+				</div>'
+		
+}
